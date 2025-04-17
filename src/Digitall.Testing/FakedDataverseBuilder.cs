3779875e90d8d4fcb4ba93881613974f18c3471e
Marcel Kummerow <Marcel.Kummerow@digitall.com>
2025-04-17 15:21:14 +0200

    feat: allow TimeProvider in FakedDataverseBuilder ctors

--- src/Digitall.Testing/FakedDataverseBuilder.cs
+++ src/Digitall.Testing/FakedDataverseBuilder.cs
@@ -7,10 +7,36 @@ namespace Digitall.Testing;
 
 public class FakedDataverseBuilder : PluginExecutionContextBuilder
 {
+    /// <summary>
+    /// Creates a new instance of <see cref="FakedDataverseBuilder"/> with a default instance of <see cref="FakedDataverse"/>.
+    /// </summary>
     public FakedDataverseBuilder()
     {
         OrganizationService = new FakedDataverse();
         OrganizationService.AddDefaultRequests();
+    }
+
+    /// <summary>
+    /// Creates a new instance of <see cref="FakedDataverseBuilder"/> with an instance of <see cref="FakedDataverse"/>
+    /// that uses the given <see cref="TimeProvider"/>.
+    /// </summary>
+    public FakedDataverseBuilder(TimeProvider timeProvider)
+    {
+        // Create a new FakedDataverse instance with the given TimeProvider
+        OrganizationService = new FakedDataverse(timeProvider);
+
+        // Add all default requests to the OrganizationService
+        OrganizationService.AddDefaultRequests();
+    }
+
+    /// <summary>
+    /// Creates a new instance of <see cref="FakedDataverseBuilder"/> with the provided <see cref="FakedDataverse"/> instance.
+    /// </summary>
+    /// <param name="fakedDataverse">An existing instance of <see cref="FakedDataverse"/> to use as the organization service.</param>
+    public FakedDataverseBuilder(FakedDataverse fakedDataverse)
+    {
+        // Assign the provided FakedDataverse instance to the OrganizationService
+        OrganizationService = fakedDataverse;
     }
 
     public new FakedDataverse OrganizationService
