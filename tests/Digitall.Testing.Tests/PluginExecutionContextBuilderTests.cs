1588aabe70edfac946d6cb002e1f9dfabc38c7ae
Lars Bauer <lars.bauer@digitall.com>
2026-03-11 11:50:13 +0100

    increase test coverage

--- tests/Digitall.Testing.Tests/PluginExecutionContextBuilderTests.cs
+++ tests/Digitall.Testing.Tests/PluginExecutionContextBuilderTests.cs
@@ -3,8 +3,9 @@ using AwesomeAssertions;
 using Digitall.Testing.Extensions;
 using Digitall.Testing.Tests.Fixtures;
 using Digitall.Testing.Tests.Fixtures.SamplePlugin;
-using Microsoft.Extensions.Time.Testing;
 using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.PluginTelemetry;
 using Microsoft.Xrm.Sdk.Query;
 using NSubstitute;
 
@@ -176,5 +177,133 @@ public class PluginExecutionContextBuilderTests
         plugin.Execute(serviceProvider);
 
         tracingService.Received().Trace("TestPlugin: Execute");
+    }
+
+    [TestMethod]
+    public void SettingModeStageAndIds_Should_SetPluginExecutionContextFields()
+    {
+        var initiatingUserId = Guid.NewGuid();
+        var correlationId = Guid.NewGuid();
+
+        var serviceProvider = new PluginExecutionContextBuilder
+        {
+            Mode = 1,
+            Stage = 40
+        }
+        .WithInitiatingUserId(initiatingUserId)
+        .WithCorrelationId(correlationId)
+        .WithMessageName(SdkMessageNames.Update)
+        .BuildServiceProvider();
+
+        var pluginContext = serviceProvider.GetService(typeof(IPluginExecutionContext)) as IPluginExecutionContext;
+
+        pluginContext.Should().NotBeNull();
+        pluginContext.Mode.Should().Be(1);
+        pluginContext.Stage.Should().Be(40);
+        pluginContext.InitiatingUserId.Should().Be(initiatingUserId);
+        pluginContext.CorrelationId.Should().Be(correlationId);
+        pluginContext.MessageName.Should().Be(SdkMessageNames.Update);
+    }
+
+    [TestMethod]
+    public void WithInputOutputSharedAndImages_Should_ExposeConfiguredCollections()
+    {
+        var input = new ParameterCollection { ["in"] = 1 };
+        var output = new ParameterCollection { ["out"] = 2 };
+        var shared = new ParameterCollection { ["shared"] = 3 };
+        var preImages = new EntityImageCollection { ["Pre"] = new Entity("account", Guid.NewGuid()) };
+        var postImages = new EntityImageCollection { ["Post"] = new Entity("account", Guid.NewGuid()) };
+
+        var serviceProvider = new PluginExecutionContextBuilder()
+            .WithInputParameters(input)
+            .WithOutputParameters(output)
+            .WithSharedVariables(shared)
+            .WithPreEntityImages(preImages)
+            .WithPostEntityImages(postImages)
+            .BuildServiceProvider();
+
+        var pluginContext = serviceProvider.GetService(typeof(IPluginExecutionContext)) as IPluginExecutionContext;
+        var pluginContext7 = serviceProvider.GetService(typeof(IPluginExecutionContext7)) as IPluginExecutionContext7;
+
+        pluginContext.Should().NotBeNull();
+        pluginContext7.Should().NotBeNull();
+        pluginContext.InputParameters.Should().BeSameAs(input);
+        pluginContext.OutputParameters.Should().BeSameAs(output);
+        pluginContext.SharedVariables.Should().BeSameAs(shared);
+        pluginContext.PreEntityImages.Should().BeSameAs(preImages);
+        pluginContext.PostEntityImages.Should().BeSameAs(postImages);
+        pluginContext7.PreEntityImagesCollection.Should().ContainSingle();
+        pluginContext7.PostEntityImagesCollection.Should().ContainSingle();
+    }
+
+    [TestMethod]
+    public void OrganizationServiceFactory_Should_ReturnConfiguredService_ForAnyUser()
+    {
+        var organizationService = Substitute.For<IOrganizationService>();
+        var serviceProvider = new PluginExecutionContextBuilder(organizationService).BuildServiceProvider();
+
+        var organizationServiceFactory = serviceProvider.GetService(typeof(IOrganizationServiceFactory)) as IOrganizationServiceFactory;
+
+        organizationServiceFactory.Should().NotBeNull();
+        organizationServiceFactory.CreateOrganizationService(Guid.NewGuid()).Should().Be(organizationService);
+        organizationServiceFactory.CreateOrganizationService(null).Should().Be(organizationService);
+    }
+
+    [TestMethod]
+    public void LoggerConstructor_Should_RegisterILogger()
+    {
+        var logger = Substitute.For<ILogger>();
+        var serviceProvider = new PluginExecutionContextBuilder(logger).BuildServiceProvider();
+
+        var loggerFromContext = serviceProvider.GetService(typeof(ILogger));
+
+        loggerFromContext.Should().NotBeNull();
+        loggerFromContext.Should().Be(logger);
+    }
+
+    [TestMethod]
+    public void GenericBuilder_Should_Default_To_RequestName_Async_Post()
+    {
+        var serviceProvider = new PluginExecutionContextBuilder<TestPlugin, CreateRequest>()
+            .BuildServiceProvider();
+
+        var pluginContext = serviceProvider.GetService(typeof(IPluginExecutionContext)) as IPluginExecutionContext;
+
+        pluginContext.Should().NotBeNull();
+        pluginContext.MessageName.Should().Be(SdkMessageNames.Create);
+        pluginContext.Mode.Should().Be(1);
+        pluginContext.Stage.Should().Be(1);
+    }
+
+    [TestMethod]
+    public void ExistingTargetParameter_Should_ThrowOnBuild_WhenTargetIsAlsoConfigured()
+    {
+        var action = () => new PluginExecutionContextBuilder()
+            .WithInputParameter("Target", new Entity("contact", Guid.NewGuid()))
+            .WithTarget(new Entity("account", Guid.NewGuid()))
+            .BuildServiceProvider();
+
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [TestMethod]
+    public void InvalidUserIdEnvVar_Should_DefaultToEmptyGuid()
+    {
+        var originalUserId = Environment.GetEnvironmentVariable("UserId");
+
+        try
+        {
+            Environment.SetEnvironmentVariable("UserId", "invalid-guid");
+
+            var serviceProvider = new PluginExecutionContextBuilder().BuildServiceProvider();
+            var pluginContext = serviceProvider.GetService(typeof(IPluginExecutionContext)) as IPluginExecutionContext;
+
+            pluginContext.Should().NotBeNull();
+            pluginContext.UserId.Should().Be(Guid.Empty);
+        }
+        finally
+        {
+            Environment.SetEnvironmentVariable("UserId", originalUserId);
+        }
     }
 }
