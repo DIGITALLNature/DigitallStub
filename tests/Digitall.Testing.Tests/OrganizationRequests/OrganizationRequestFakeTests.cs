0187a3e1952ae5a1e75725b24d9a28ab8aa2e32e
Marcel Kummerow <Marcel.Kummerow@digitall.com>
2026-05-20 21:13:50 +0200

    chore: clean new quodana issues

--- tests/Digitall.Testing.Tests/OrganizationRequests/OrganizationRequestFakeTests.cs
+++ tests/Digitall.Testing.Tests/OrganizationRequests/OrganizationRequestFakeTests.cs
@@ -53,7 +53,7 @@ public class OrganizationRequestFakeTests
     }
 
     [Test]
-    public async Task DeleteFake_Should_RemoveRecord()
+    public Task DeleteFake_Should_RemoveRecord()
     {
         _sut.AddRequest(new DeleteFake());
         var id = Guid.NewGuid();
@@ -65,6 +65,7 @@ public class OrganizationRequestFakeTests
 
         void Action() => _sut.Retrieve(Account.EntityLogicalName, id, new ColumnSet(true));
         Assert.Throws<FaultException<OrganizationServiceFault>>(Action);
+        return Task.CompletedTask;
     }
 
     [Test]
@@ -77,7 +78,7 @@ public class OrganizationRequestFakeTests
         var request = new RetrieveRequest
         {
             Target = new EntityReference(Account.EntityLogicalName, id),
-            ColumnSet = new Microsoft.Xrm.Sdk.Query.ColumnSet("name")
+            ColumnSet = new ColumnSet("name")
         };
 
         var response = (RetrieveResponse)_sut.Execute(request);
@@ -231,7 +232,7 @@ public class OrganizationRequestFakeTests
         var response = (ExecuteTransactionResponse)_sut.Execute(request);
 
         await Assert.That(response.Responses.Count).IsEqualTo(2);
-        var accounts = _sut.RetrieveMultiple(new Microsoft.Xrm.Sdk.Query.QueryExpression(Account.EntityLogicalName) { ColumnSet = new Microsoft.Xrm.Sdk.Query.ColumnSet(true) });
+        var accounts = _sut.RetrieveMultiple(new QueryExpression(Account.EntityLogicalName) { ColumnSet = new ColumnSet(true) });
         await Assert.That(accounts.Entities).Count().IsEqualTo(2);
     }
 
@@ -250,10 +251,10 @@ public class OrganizationRequestFakeTests
         {
             JobName = "Bulk Delete Test",
             QuerySet = [
-                new Microsoft.Xrm.Sdk.Query.QueryExpression(Account.EntityLogicalName) {
-                    Criteria = new Microsoft.Xrm.Sdk.Query.FilterExpression {
+                new QueryExpression(Account.EntityLogicalName) {
+                    Criteria = new FilterExpression {
                         Conditions = {
-                            new Microsoft.Xrm.Sdk.Query.ConditionExpression("name", Microsoft.Xrm.Sdk.Query.ConditionOperator.Equal, "Delete Me")
+                            new ConditionExpression("name", ConditionOperator.Equal, "Delete Me")
                         }
                     }
                 }
@@ -267,10 +268,10 @@ public class OrganizationRequestFakeTests
 
         await Assert.That(response.Results.ContainsKey("JobId")).IsTrue();
 
-        void RetrieveDeleted() => _sut.Retrieve(Account.EntityLogicalName, id1, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
+        void RetrieveDeleted() => _sut.Retrieve(Account.EntityLogicalName, id1, new ColumnSet(true));
         Assert.Throws<FaultException<OrganizationServiceFault>>(RetrieveDeleted);
 
-        var kept = _sut.Retrieve(Account.EntityLogicalName, id2, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
+        var kept = _sut.Retrieve(Account.EntityLogicalName, id2, new ColumnSet(true));
         await Assert.That(kept).IsNotNull();
     }
 
@@ -279,7 +280,7 @@ public class OrganizationRequestFakeTests
     {
         _sut.AddRequest(new RetrieveEntityFake());
         var metadata = new EntityMetadata { LogicalName = Account.EntityLogicalName };
-        _sut.EntityMetadata.Add(Account.EntityLogicalName, metadata);
+        _sut.State.EntityMetadata.Add(Account.EntityLogicalName, metadata);
 
         var request = new RetrieveEntityRequest { LogicalName = Account.EntityLogicalName };
 
